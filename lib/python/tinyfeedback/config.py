f2ec3757cf1c51b1312f70317d92241266033ef0
Zach Steindler <steiza@coffeehousecoders.org>
2011-03-22 00:16:48 -0400

    setting up with distutils

--- lib/python/tinyfeedback/config.py
+++
@@ -1,6 +0,0 @@
-PORT = 8000
-
-LOG_PATH = '/var/log/tinyfeedback.log'
-LOG_LEVEL = 'DEBUG'
-
-DATA_STORE = 'mysql://root@localhost/tinyfeedback'
---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name='tiny_feedback',
+      version='0.1',
+      author='Zach Steindler',
+      author_email='steiza@gmail.com',
+      url='https://github.com/steiza/tinyfeedback',
+      description="A simple graphing web-based dashboard",
+      long_description="""\
+tinyfeedback is a ridiculously simple way for you to see trends in whatever you are monitoring. You do an HTTP POST to put data in, and you point and click in the web interface to make some graphs. Yay!""",
+      keywords='dashboard, web, graph, metrics',
+      classifiers=['Programming Language :: Python', 'License :: OSI Approved :: BSD License'], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      license='BSD',
+      packages=['tinyfeedback'],
+      scripts=['bin/tinyfeedback', 'bin/tinyfeedback-ctl'],
+      )
